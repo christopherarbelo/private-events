6fe6880f3706bedc9d85d5e95f0241cdb7ec14dd
Christopher Arbelo <christopherarbelo@gmail.com>
2021-03-23 20:17:39 -0400

    Create Invitation through table and hook up the association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :hosted_events, class_name: 'Event', foreign_key: 'host_id', dependent: :destroy
+  has_many :invitations, foreign_key: 'guest_id'
+  has_many :invited_events, through: :invitations, source: :event
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_222706) do
+ActiveRecord::Schema.define(version: 2021_03_23_232538) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "date"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2021_03_22_222706) do
     t.index ["host_id"], name: "index_events_on_host_id"
   end
 
+  create_table "invitations", force: :cascade do |t|
+    t.integer "guest_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_invitations_on_event_id"
+    t.index ["guest_id"], name: "index_invitations_on_guest_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -29,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_03_22_222706) do
   end
 
   add_foreign_key "events", "users", column: "host_id"
+  add_foreign_key "invitations", "users", column: "guest_id"
 end
